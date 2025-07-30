bfa647018523b3c145fbe73f8475f68d72c49da8
rociocjz <rociocojulum@gmail.com>
2025-07-29 21:01:47 -0600

    cambios

--- # Responder con liuneas de codigo numero.py
+++ # Responder con liuneas de codigo numero.py
@@ -6,3 +6,23 @@ while True:
         break
     suma += numero
 print("la suma es:", suma)
+
+
+print("EJERCICIO 2 LISTADO DE NUMEROS")
+numeros = [10, 20, 30, 40]
+for i in numeros:
+    print(i * 2)
+
+
+print("EJERCICIO 3 LISTADO DE NUMEROS")
+
+productos =[]
+for i in range(3):
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio: "))
+    stock = int(input("stock: "))
+    producto = {"nombre":  nombre, "precio": precio, "stock": stock}
+    productos.append(producto)
+
+for x in productos:
+   print(f"{x['nombre']}: Q{x['precio']} ({x['stock']} disponibles)")
