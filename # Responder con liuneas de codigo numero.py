942ced8b2ee2c027642758f2ceb00d712dd449be
rociocjz <rociocojulum@gmail.com>
2025-07-29 20:35:38 -0600

    otro cambio

--- # Responder con liuneas de codigo numero.py
+++ # Responder con liuneas de codigo numero.py
@@ -1,19 +1,8 @@
-# Responder con liuneas de codigo numero uno crear una liusta con nombres
+suma = 0
 
-nombres = ["Ana", "Luis", "Pedro"]
-
-# NUMERO 2
-#ya tengo la lista ahora para eliminar un elemento se pueden usar diversos metodos 
-# En este caso usare remove eligiendo un nombre de los que pedian
-
-nombres.remove("Ana")
-print(nombres)
-
-#de esta manera se puede eliminar un elemento de una lista
-
-#NUMERO 3
-# En la pregunta numero 3  pregunta que hace el codigo que se muestra en el archivo
-# El ciclo while true hace que la codicion siempre sea verdadera y el break que
-#Se rompa el ciclo asi el Hola solo se mostrara 1 vez de lo contrario se mostrara de manera infinita
-
-#NUMERO 4 declarar un diccionario con mi nombre, edad y carrera
+while True:
+    numero = int(input("Ingrese un numero que no sea 0: "))
+    if numero == 0:
+        break
+    suma += numero
+print("la suma es:", suma)
